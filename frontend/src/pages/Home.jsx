7ede6e2527b7b893b5b56a8626617197bce922ed
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-17 06:54:46 +0530

    created book-appointment section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
-      <TopDoctors/>
+      <TopDoctors />
+      <Banner/>
     </div>
   )
 }
