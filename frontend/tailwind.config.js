4a2a8ff9b2ab2be9d0c38379cbb71202a5f49ee5
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-17 06:35:59 +0530

    smooth-scroll

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx,html}",
-    "./public/index.html"
+    "./public/index.html",
   ],
   theme: {
     extend: {
       colors: {
-        'primary': "#5f6FFF",
-        
+        primary: "#5f6FFF", // ✅ Removed trailing comma
       },
       gridTemplateColumns: {
-        'auto':'repeat(auto-fill,minmax(200px,1fr))'
-      }
+        auto: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
     },
   },
   plugins: [],
-}
-
+};
