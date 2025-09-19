ec6535271f7c55eeeeb6f8f2ee9f3663cb10238a
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-19 07:20:36 +0530

    created doctors page

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,11 +1,19 @@
-import React, { useContext } from 'react'
-// import { doctors } from '../assets/assets' //& used App context
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
+import { useNavigate,  } from 'react-router-dom'
 
-const TopDoctors = () => {
+const RelatedDoctors = ({speciality , docId}) => {
+    const { doctors } = useContext(AppContext)
     const navigate = useNavigate()
-    const {doctors} = useContext(AppContext)
+    
+    const [relDoc, setRelDoc] = useState([])
+    // * so using these props we will filter the related doctors from doctor and store it into reldoc
+    useEffect(() => {
+        if (doctors.length > 0 && speciality) {
+            const doctorsData = doctors.filter((doc) => doc.speciality === speciality && doc._id !== docId)
+            setRelDoc(doctorsData)
+        }
+    },[doctors,speciality,docId])
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -13,12 +21,9 @@ const TopDoctors = () => {
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {relDoc.slice(0, 5).map((item, index) => (
           <div
-            onClick={() => {
-              navigate(`/appointment/${item._id}`);
-              scrollTo(0, 0);
-            }}
+                onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
             key={index}
             className="border border-blue-200 rounded-t-xl overflow-hidden cursor-pointer hover:translate-y-[10px] transition-all duration-500"
           >
@@ -47,4 +52,4 @@ const TopDoctors = () => {
   );
 }
 
-export default TopDoctors
+export default RelatedDoctors
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {AppContext} from '../context/AppContext'
 import { assets } from '../assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -161,6 +162,9 @@ const Appointment = () => {
           </div>
           <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 ' >Book Appointment</button>
         </div>
+
+        {/* RELATED DOCTORS SECTIONS */}
+        <RelatedDoctors docId={docId} speciality={ docInfo.speciality} />
       </div>
     )
   );
