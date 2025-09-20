6c8733ecea40d7e91aea9070c15bbf2dc2ec7552
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-20 06:22:58 +0530

    started witg nyprofile page creation

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: "Yukti sahu",
+    image: assets.profile_pic,
+    email: 'yuktisahu@gmail.com',
+    phone:'+1 123 456 7890'
+  })
   return (
     <div>
       
