0cedc15a1b3451fd09ef1edcc33d0a23e2910ee5
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-25 11:17:28 +0530

    login page designed and updations

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-export const AppContext = createContext
+export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const value = {
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
   return (
     <form
       onSubmit={onSubmitHandler}
-      className="min-h-[80vh] flex items-center">
+      className="min-h-[80vh] flex flex-col justify-center items-center">
       <div className="flex flex-col gap-3 items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg ">
         <p className="text-2xl font-semibold m-auto">
           <span className="text-primary">{state}</span> Login
