19a3184cf46d230cde42e0871e7da300bb980364
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-09-25 23:12:01 +0530

    started with doctor lists

--- admin/src/pages/admin/DoctorsList.jsx
+++ admin/src/pages/admin/DoctorsList.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
+  
+  const { doctor, aToken, getAllDoctors } = useContext(AdminContext)
+  
+
+  useEffect(() => {
+    if (aToken){
+      getAllDoctors()
+    }
+  }, [aToken ])
   return (
-    <div>DoctorsList</div>
+    <div>
+      <h1>All Doctors</h1>
+      <div>
+        {
+          doctor.map((item,index) => (
+            <div key={index}>
+              <img src={item.image} alt="" />
+              <div>
+                <p>{ item.image}</p>
+                <p>{ item.speciality}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
