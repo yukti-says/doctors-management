2ec625d2bc585b400e8cba50287c7eff3f493ff1
yukti-says <142389696+yukti-says@users.noreply.github.com>
2025-11-22 19:44:46 +0530

    preparing for doctors dashboards

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext  } from 'react'
+import React, { useContext  } from 'react'
 import Login from './pages/Login'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -11,24 +11,34 @@ import Dashboard from './pages/admin/Dashboard'
 import AllAppointments from './pages/admin/AllAppointments'
 import AddDoctor from './pages/admin/AddDoctor'
 import DoctorsList from './pages/admin/DoctorsList'
+import { DoctorsContext } from './context/DoctorsContext'
+import DoctorDashboard from './pages/doctor/DoctorDashboard'
+import DoctorAppointment from './pages/doctor/doctorAppointment'
+import DoctorProfile from './pages/doctor/DoctorProfile'
 
 const App = () => {
 
-const {aToken} = useContext(AdminContext)
+  const { aToken } = useContext(AdminContext)
+  const {dToken} = useContext(DoctorsContext)
 
 
-  return aToken ? (
+  return aToken || dToken ? (
     <div className="bg-[#F8F9FD">
       <ToastContainer />
       <Navbar />
       <div className="flex items-start ">
         <Sidebar />
         <Routes>
+          {/* admin routes */}
           <Route path="/" element={<></>} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
           <Route path="/all-appointments" element={<AllAppointments />} />
           <Route path="/add-doctor" element={<AddDoctor />} />
           <Route path="/doctor-list" element={<DoctorsList />} />
+          {/* doctor routes */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointment />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
         </Routes>
       </div>
     </div>
---
+++ admin/src/pages/doctor/DoctorAppointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorAppointment = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorAppointment
